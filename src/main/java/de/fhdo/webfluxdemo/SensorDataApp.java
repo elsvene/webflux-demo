655153937c1ec686765e1260a2727c8b31dd51bc
Sven Jörges <sven.joerges@fh-dortmund.de>
2021-12-09 12:26:44 +0100

    Added implementation via websockets

--- src/main/java/de/fhdo/webfluxdemo/SensorDataApp.java
+++ src/main/java/de/fhdo/webfluxdemo/SensorDataApp.java
@@ -1,13 +1,27 @@
 package de.fhdo.webfluxdemo;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
 
 @SpringBootApplication
 public class SensorDataApp {
+
+	@Autowired
+	private WebSocketHandler webSocketHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SensorDataApp.class, args);
 	}
 
+	@Bean
+	public HandlerMapping webSocketHandlerMapping() {
+		return new SimpleUrlHandlerMapping(Map.of("/ws/stream", webSocketHandler), 1);
+	}
 }
---
+++ src/main/java/de/fhdo/webfluxdemo/controller/SensorDataWebSocketHandler.java
@@ -0,0 +1,44 @@
+package de.fhdo.webfluxdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.fhdo.webfluxdemo.domain.SensorData;
+import de.fhdo.webfluxdemo.service.SensorDataService;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class SensorDataWebSocketHandler implements WebSocketHandler {
+	private SensorDataService service;
+	private ObjectMapper mapper;
+
+	@Autowired
+	public SensorDataWebSocketHandler(SensorDataService service, ObjectMapper mapper) {
+		this.service = service;
+		this.mapper = mapper;
+	}
+
+	@Override
+	public Mono<Void> handle(WebSocketSession session) {
+		Flux<String> messages = service.findTemperatureData().map(this::convertToJsonString);
+		return session.send(messages.map(session::textMessage))
+				.and(session.receive().map(WebSocketMessage::getPayloadAsText).log());
+	}
+
+	private String convertToJsonString(SensorData data) {
+		String result;
+		try {
+			result = mapper.writeValueAsString(data);
+		} catch (JsonProcessingException e) {
+			result = "";
+		}
+		return result;
+	}
+}
